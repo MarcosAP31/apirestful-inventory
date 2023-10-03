e438ea9b74859e09f9a5ebd805134062ef4809c1
MarcosAP31 <marcos313art@gmail.com>
2023-10-03 07:56:30 -0500

    03oct

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -2,7 +2,7 @@ export default {
     database: {
         host: '127.0.0.1',
         user: 'marcos',
-        password: 'Amkl-572#$LCVy',
+        password: 'SQLPassword',
         database: 'virtualstore'
     }
 }
